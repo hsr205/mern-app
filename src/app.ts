c5a1a158ac3561a73caf8a975a2b99d0faf60f39
Henry Rothenberg <hsr205@aol.com>
2024-12-13 15:37:22 -0500

    Add error handling / endpoint / mongodb interaction

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
-import express, {Express} from "express";
+import express, {Request, Response, Express, NextFunction} from "express";
+import NoteModel from "./models/notes";
+import pino, {Logger} from "pino";
 
 dotenv.config();
+
+const logger: Logger = pino({})
 
 const app: Express = express();
 
@@ -9,6 +13,45 @@ const app: Express = express();
 // The same logic can be used to create any endpoint required
 app.get("/", (request, response) => {
     response.send("Hello World!");
+});
+
+app.get("/notes", async (request, response, next) => {
+    try {
+        // Looks through MongoDB to find all "Notes" instances
+        // .exec() returns a promise
+        // await is an async keyword
+        const notes = await NoteModel.find().exec();
+        response.status(200).json(notes);
+    } catch (error) {
+        // Calls the exception handler THAT IS DIRECTLY AFTER THIS CODE BLOCK
+        next(error);
+    }
+});
+
+// In the event that the user passes in an endpoint that doesn't exist, this will pass a HTTP 404 exception
+app.use((request: Request, response: Response, next: NextFunction) => {
+    response.status(404).json(
+        {
+            error: "Page Not Found: HTTP 404",
+            status: 404,
+        }
+    );
+})
+
+// Acts as a modular exception handler
+app.use((error: unknown, request: Request, response: Response, nextFunction: NextFunction) => {
+    if (error instanceof Error) {
+        // Logs the exception to the terminal
+        logger.error(`Exception thrown: ${error.message}`);
+
+        // Provides a JSON response to the user at the endpoint expressed above
+        response.status(500).json(
+            {
+                error: error.message,
+                status: 500,
+            }
+        );
+    }
 });
 
 export default app;
