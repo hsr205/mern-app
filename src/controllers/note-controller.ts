8a78fa3c76349aeca95d66f66423478d6105d731
Henry Rothenberg <hsr205@aol.com>
2024-12-15 11:51:13 -0500

    Add methods

--- src/controllers/note-controller.ts
+++ src/controllers/note-controller.ts
@@ -13,3 +13,42 @@ export const getNotes: RequestHandler = async (request, response, next) => {
         next(error);
     }
 }
+
+export const getNote: RequestHandler = async (request, response, next) => {
+    try {
+        // Looks through MongoDB to find all "Notes" instances
+        // .exec() returns a promise
+        // await is an async keyword
+        const noteId: string = request.params.noteId;
+
+        const note = await NoteModel.findById(noteId).exec();
+        response.status(200).json(note);
+    } catch (error) {
+        // Calls the exception handler THAT IS DIRECTLY AFTER THIS CODE BLOCK
+        next(error);
+    }
+}
+
+export const createNote: RequestHandler = async (request, response, next) => {
+
+
+    try {
+        // Stores the title and text of the note being created by the user on the front end
+        const noteTitle: string = request.body.noteTitle;
+        const noteText: string = request.body.noteText;
+
+        // Passes the newly created note to the Model and returns a promise
+        // This is why we use the async "await" key word
+        const newNote = await NoteModel.create({
+            noteTitle: noteTitle,
+            noteText: noteText
+        });
+
+        response.status(201).json(newNote);
+
+    } catch (error) {
+        // Calls the exception handler THAT IS DIRECTLY AFTER THIS CODE BLOCK
+        next(error);
+    }
+
+};
