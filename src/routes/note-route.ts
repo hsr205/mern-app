4d05e6a48ff365c5bffde72da78b292a802bf6e2
Henry Rothenberg <hsr205@aol.com>
2024-12-15 11:51:30 -0500

    Add endpoint

--- src/routes/note-route.ts
+++ src/routes/note-route.ts
@@ -3,6 +3,11 @@ import * as NotesController from "../controllers/note-controller"
 
 const router = express.Router();
 
+
 router.get("/", NotesController.getNotes);
+
+router.get("/:noteId", NotesController.getNote);
+
+router.post("/", NotesController.createNote);
 
 export default router;
