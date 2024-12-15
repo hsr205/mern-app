8f74c22029cd3887e0d417fe2342d76717f668fb
Henry Rothenberg <hsr205@aol.com>
2024-12-15 11:50:43 -0500

    Remove logger

--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,18 @@ dotenv.config();
 
 import env from "./util/validateEnv";
 import app from "./app";
-import pino, {Logger} from 'pino';
 import mongoose from "mongoose";
 
 const port: number = env.DEFAULT_PORT_NUM;
-const logger: Logger = pino({})
 
 mongoose.connect(env.MONGO_DB_CONNECTION_STR)
     .then(() => {
-        logger.info("Connected to MongoDB")
+        console.log("Connected to MongoDB")
         app.listen(process.env.DEFAULT_PORT_NUM, () => {
             console.log(`Server started on port ${port}`);
         })
     })
     .catch((err) => {
-        logger.error(`MongoDB Connection exception thrown: ${err.message}`);
+        console.log(`MongoDB Connection exception thrown: ${err.message}`);
     });
 
