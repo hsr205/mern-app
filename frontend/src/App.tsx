cd9dc5299a35f45082be4a505ffc66acfd7c7b21
Henry Rothenberg <hsr205@aol.com>
2025-01-03 10:18:57 -0500

    Add button to UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "react-bootstrap";
 
 function App() {
+
+    let [counter, setCounter] = React.useState(0);
+
+    function incrementCounter(): void {
+        setCounter(counter + 1);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                    Hello from React App!
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+
+                <Button onClick={() => incrementCounter()}>
+                    Click Count: {counter}
+                </Button>
             </header>
         </div>
     );
