8625389df83dbc6c256309de75f4ef6d0700c034
Henry Rothenberg <hsr205@aol.com>
2025-01-05 16:04:16 -0500

    Add React logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
-import {Note} from "./models/note"
+import {Note as NoteModel} from "./models/note"
+import NoteComponent from "./components/Note.tsx"
+import {Container, Row} from "react-bootstrap";
 
 function App() {
 
-    let [notes, setNotes] = React.useState<Note[]>([]);
+    let [notes, setNotes] = React.useState<NoteModel[]>([]);
 
     React.useEffect(() => {
         async function loadNotes() {
@@ -23,9 +24,16 @@ function App() {
 
 
     return (
-        <div className="App">
-            {JSON.stringify(notes)}
-        </div>
+        <Container>
+            <Row xs={1} md={2} xl={3}>
+
+
+                {notes.map(note => (
+                    <NoteComponent note={note} key={note._id}/>
+                ))}
+
+            </Row>
+        </Container>
     );
 }
 
